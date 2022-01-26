3cace5d983d24623c9cdbbadd510987cf6393ac3
Nicolas Aguirre Yacup <nicolas.aguirre.y@gmail.com>
2022-01-25 23:13:19 -0500

    trabajo inicio en el contenido del cv

--- src/containers/CvNicolas.jsx
+++ src/containers/CvNicolas.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import '../styles/CvNicolas.css'
 
 const CvNicolas = () => {
     return (
-        <div>
-            <h1>Hola a mi nuevo CV</h1>
+        <div id='container'>
+            <div className="row">
+                <img id='myImage' src="../assets/images/me.jpg" alt="" />
+                <div id='containerTextName'>
+                <span id='textBox' class="border border-dark fs-1 fw-bold">Nicolas Aguirre Yacup
+                
+                </span>
+                </div>
+            </div>
+            <div className="row">
+                <section>
+                    <span>Experiencia</span>
+                </section>
+                <section>
+                    <span>educacion</span>
+                </section>
+            </div>
         </div>
     );
 };
