53cfe1f70f593336de28ed05dbacbe3e4dea91be
Nicolas Aguirre Yacup <nicolas.aguirre.y@gmail.com>
2022-01-26 23:33:34 -0500

    update CvNicolas, new font added

--- src/containers/CvNicolas.jsx
+++ src/containers/CvNicolas.jsx
@@ -4,21 +4,30 @@ import '../styles/CvNicolas.css'
 const CvNicolas = () => {
     return (
         <div id='container'>
-            <div className="row">
+            <div id='containerImg' className="row">
                 <img id='myImage' src="../assets/images/me.jpg" alt="" />
-                <div id='containerTextName'>
-                <span id='textBox' class="border border-dark fs-1 fw-bold">Nicolas Aguirre Yacup
-                
+                <div id='containerTextName' className="border border-dark fs-2 fw-bold">
+                <span id='textBox'>Nicolas Aguirre Yacup
+                <br />
+                <h2>Software Developer</h2>
                 </span>
                 </div>
             </div>
-            <div className="row">
-                <section>
-                    <span>Experiencia</span>
-                </section>
-                <section>
-                    <span>educacion</span>
-                </section>
+            <div className="container bg-dark">
+                <div id='' className="row">
+                    <div className="col">
+                        <section id='experiancia'>
+                            {/* <span>Experiencia</span> */}
+                            <h1>Experience</h1>
+                        </section>
+                    </div>
+                    <div className="col">
+                        <section id='educaion'>
+                            {/* <span>educacion</span> */}
+                            <h1>Education</h1>
+                        </section>
+                    </div>
+                </div>
             </div>
         </div>
     );
