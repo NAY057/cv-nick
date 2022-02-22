1e836d687f803163dffa9990b178cd8c385b24bf
Nicolas Aguirre Yacup <nicolas.aguirre.y@gmail.com>
2022-02-22 00:05:57 -0500

    hotfix layout

--- src/containers/Layout.jsx
+++ src/containers/Layout.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import Header from '../components/Header.jsx'
 import Footer from '../components/Footer.jsx'
-
+import Home from '../pages/Home.jsx'
 
 const Layout = ({children}) => {
     return(
         <div>
             <Header/>
-            {children}
+            <Home/>
+            {/* {children} */}
             <Footer/>
         </div>
     )
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -3,14 +3,14 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Layout from '../containers/Layout.jsx';
 import '../styles/Global.css';
 // import '../styles/App.css';
-import Home from '../pages/Home.jsx'
+// import Home from '../pages/Home.jsx'
 
 const App = () => {
   return (
     <BrowserRouter>
       <Layout>
         <Routes>
-          <Route exact path='/' element={<Home/>} />
+          {/* <Route exact path='/' element={<Home/>} /> */}
         </Routes>
       </Layout>
     </BrowserRouter>
