2329a6eab7ac4537a29cc2466200018b7e23be8f
Nicolas Aguirre Yacup <nicolas.aguirre.y@gmail.com>
2022-02-21 23:35:17 -0500

    Media queries added for mobile and tablet

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,7 +14,7 @@ const Footer = () => {
                 <div className="card mb-3"></div>
                 <div className="row">
                     <div className="col">
-                        <p className="container">
+                        <p id='footerInfo' className="container">
                             <img id='imgfooter' src={linkedin} alt="LinkedIn" />: <a href="https://goo.su/9tKY" rel="noopener noreferrer" target="_blank">Nicolas Aguirre Yacup</a> <br />
                             <img id='imgfooter' src={mobile} alt="Mobile Phone" />: (+57) 318-547-0851 <br />
                             <img id='imgfooter' src={whatsapp} alt="WhatsApp" />: <a href="https://walink.co/687925" rel="noopener noreferrer" target="_blank"> (+57) 315-462-7068 </a> <br />
@@ -24,11 +24,13 @@ const Footer = () => {
                             <img id='imgfooter' src={git} alt="GitHub" />: <a href="https://github.com/NAY057" rel="noopener noreferrer" target="_blank">NAY057</a><br />
                         </p>
                     </div>
-                    <div  id='bt_download_col' className="col">
-                    <a id='bt_download' type="button" class="btn btn-warning btn-lg" href={myCV} rel="noopener noreferrer" target="_blank" download="CV nicolas aguirre yacup.pdf"><strong>Download CV Here</strong>  </a>
+                    <div id='bt_download_col' className="col">
+                        <a id='bt_download' type="button" class="btn btn-warning btn-lg" href={myCV} rel="noopener noreferrer" target="_blank" download="CV nicolas aguirre yacup.pdf"><strong>Download CV Here</strong>  </a>
                     </div>
                 </div>
-
+                <div class="text-center">
+                    <p>Desined with ❤ by Nicolas Aguirre Yacup - 2022</p>
+                </div>
             </div>
         </div>
     );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -26,7 +26,47 @@ const Header = () => {
                     </li>
                 </ul>
             </div>
+        
+
+            <div class="collapse" id="navbarToggleExternalContent">
+            <div class="bg-dark p-4">
+                <h5 class="text-white h4">Menu</h5>
+                {/* <span class="text-muted">Toggleable via the navbar brand.</span> */}
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <a className="nav-link active" aria-current="page" href="/">Me</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#experience">Experience</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#skllis">Skills</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#education">Education</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#languages">Languages</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#footer">Contact</a>
+                    </li>
+                </ul>
+            </div>
+            </div>
+            <nav id='nav_btn_burguer' class="navbar navbar-dark bg-dark">
+            <div class="container-fluid">
+                <button id='btn_burguer' class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+            </div>
+            </nav>
+
+
         </nav>
+
+        
+        
     );
 };
 
